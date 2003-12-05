f7f35a1d02035a4891357c07e69d0a8cc5c9e5d3
reaper <reaper@f1d075ae-1311-0410-a175-e1b5f372ae4b>
2003-12-05 09:23:55 +0000

    Fixed file paths.

--- scripts/make_navbar.pl
+++ scripts/make_navbar.pl
@@ -4,6 +4,21 @@
 #
 ################################################################################
 
+use File::Spec('abs2rel','canonpath');
+
+$htmldir = $ARGV[0];
+$f95docdir = $ARGV[1];
+$docdir = $ARGV[2];
+
+opendir DIR,$htmldir;
+@dirlist = grep(/_short\.html$/,readdir(DIR));
+closedir DIR;
+
+$rel_path = File::Spec->abs2rel($htmldir,$docdir);
+$htmldir = File::Spec->canonpath($rel_path);
+$rel_path = File::Spec->abs2rel($f95docdir,$docdir);
+$f95docdir = File::Spec->canonpath($rel_path);
+
 print "<HTML>\n";
 print "<HEAD>\n";
 print "  <META HTTP-EQUIV=\"Content-Type\" CONTENT=\"text/html; charset=iso-8859-1\">\n";
@@ -31,9 +46,6 @@ print "</DIV>\n";
 print "<BR>\n";
 print "<DIV STYLE=\"background-color : #DDDDEE\">MODULES</DIV>\n";
 
-opendir DIR,$ARGV[0];
-@dirlist = grep(/_short\.html$/,readdir(DIR));
-
 foreach $i (@dirlist) {
   $_=$i;
   s/_short\.html$//go;
@@ -43,14 +55,14 @@ foreach $i (@dirlist) {
 @proglist = grep(/^run_/o,@filelist);
 
 foreach $i (@modlist) {
-   print "<BR><A HREF=\"htmlfiles/$i\_short.html\">$i</A> <A HREF=\"htmlfiles/$i.html\">.foo</A>  <A HREF=\"f95files/$i.F95\">.F95</A>\n";
+   print "<BR><A HREF=\"$htmldir/$i\_short.html\">$i</A> <A HREF=\"$htmldir/$i.html\">.foo</A>  <A HREF=\"$f95docdir/$i.F95\">.F95</A>\n";
 }
 
 print "<BR><BR>\n";
 print "<DIV STYLE=\"background-color : #DDDDEE\">PROGRAMS</DIV>\n";
 
 foreach $i (@proglist) {
-   print "<BR><A HREF=\"htmlfiles/$i\_short.html\">$i</A> <A HREF=\"foofiles/$i.foo\">.foo</A>  <A HREF=\"f95files/$i.F95\">.F95</A>\n";
+   print "<BR><A HREF=\"$htmldir/$i\_short.html\">$i</A> <A HREF=\"foofiles/$i.foo\">.foo</A>  <A HREF=\"$f95docdir/$i.F95\">.F95</A>\n";
 }
 
 print "<BR><BR>\n";
