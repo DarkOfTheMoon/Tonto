ae73eef113d29a6d6f976690c1748e9e8868a044
reaper <reaper@f1d075ae-1311-0410-a175-e1b5f372ae4b>
2004-09-22 04:35:35 +0000

    Adding scripts for the test suite.

---
+++ scripts/compare_stdout.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+# This script compares two stdout files.
+# It ignores predefined differences, such as times printed out, version number,
+# and platform identifier.
+use strict;
+
+my $filenamea = shift;
+my $filenameb = shift;
+
+open(FILEa,$filenamea);
+open(FILEb,$filenameb);
+
+my $equal = 1;
+while(my $lineb = <FILEb>) {
+  my $linea = <FILEa>;
+
+  # if EOF on $filea, then the files are not equal.
+  if (not defined $linea) {$equal = 0; last;}
+
+  # If they are equal, then move onto the next line.
+  if ($linea eq $lineb) {next;}
+
+  # Parts of these lines should not be compared.
+  if ($linea =~ m'^\s*Version: ') { $linea = $&}
+  if ($lineb =~ m'^\s*Version: ') { $lineb = $&}
+  if ($linea =~ m'^\s*Platform: ') { $linea = $&}
+  if ($lineb =~ m'^\s*Platform: ') { $lineb = $&}
+  if ($linea =~ m'^\s*Build-date: ') { $linea = $&}
+  if ($lineb =~ m'^\s*Build-date: ') { $lineb = $&}
+  if ($linea =~ m'^\s*Timer started at ') { $linea = $&}
+  if ($lineb =~ m'^\s*Timer started at ') { $lineb = $&}
+  if ($linea =~ m'^\s*Wall-clock time taken for job "(.*)" is ') { $linea = $&}
+  if ($lineb =~ m'^\s*Wall-clock time taken for job "(.*)" is ') { $lineb = $&}
+  if ($linea =~ m'^\s*CPU time taken for job "(.*)" is ') { $linea = $&}
+  if ($lineb =~ m'^\s*CPU time taken for job "(.*)" is ') { $lineb = $&}
+
+  if ($linea ne $lineb) {
+    $equal = 0;
+    last;
+  }
+}
+
+my $linea = <FILEa>;
+# if EOF on $filea, then the files are not equal.
+if (defined $linea) {$equal = 0;}
+
+#if ($equal) {
+#  print "$filenamea and $filenameb are equal\n";
+#} else {
+#  print "$filenamea and $filenameb are not equal\n";
+#}
+
+close(FILEb);
+close(FILEa);
+
+exit (! $equal);
