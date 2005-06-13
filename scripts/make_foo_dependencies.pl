cc5fad0007bca619f0c0676b25d91e064ee2c35a
dylan <dylan@f1d075ae-1311-0410-a175-e1b5f372ae4b>
2005-06-13 14:35:53 +0000

    Removed all tonto references, replaced with macros, so NO_TONTO_SYSTEM_CALLS really works. Added NO_TONTO_SYSTEM_CHECKS which preserves parallel calls, but not tested. Added names inheritance e.g. get_from(A:b) for routine b in module A, top inherit from differently named routines, fixed make_foo_dependencies script, finalised optimum compile switches for ifort compiler (-ipo) option.

--- scripts/make_foo_dependencies.pl
+++ scripts/make_foo_dependencies.pl
@@ -145,7 +145,7 @@ sub get_deps {
 print STDERR @gets;
   while (<FILE>) {
     if (m'get_from'io) {
-      /(?:.+):::(?:.+)get_from\(\s*([^ )]+)\s*[),]/io && do { $x=$1; push(@gets, lc($x))};
+      /(?:.+):::(?:.+)get_from\(\s*([^ ):]+)\s*[),]/io && do { $x=$1; push(@gets, lc($x))};
     }
   }
   close(FILE);
