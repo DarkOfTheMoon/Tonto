745149d620b6683f8a1c2c09973a28b4d7b1527f
dylan_ <dylan_@f1d075ae-1311-0410-a175-e1b5f372ae4b>
2012-08-20 04:14:23 +0000

    . First attempt at new version checkin . molecule has been split into several parts . foo.pl modified to deal with new submodule syntax . tests updated, still a couple of major errors

--- trunk/tonto/scripts/make_f95_dependencies.pl
+++ trunk/tonto/scripts/make_f95_dependencies.pl
@@ -1,85 +1,106 @@
-# This is a perl script to generate dependency files from Fortran 95 source
-# code.  The script looks for "program", "module", "use", "include" and
-# "#include" keywords.
+# ============================================================================
 #
-# The object file that is made from the source file has the same name as the
-# source file but with ".o" extension.  If the source file is for a program,
-# then the extension is ".exe" or what is overriden with the -prog_ext argument.
+# This perl script makes dependency files from Fortran 95 source code.
 #
-# The default module extension is ".mod", but this can be overridden with the
-# -mod_ext command line argument.
-#
-# The dependency on the source file is the full filename given to the script.
-# Object files are prefixed with the $(objdir) directory.
-# Module files are prefixed with the $(moddir) directory.
-# Program files are prefixed with the $(bindir) directory.
-# The $(objdir), $(moddir) and $(bindir) are to be set in the makefile. eg,
-#       $(moddir) := /path/to/modules
-#
-# By default, module names from "use" statements are output in lowercase, but
-# this can be overridden for those compilers that do uppercase instead.
-#
-# The script can handle multiple modules in the source file, in which case they
-# are all targets in the single "make" dependency rule.  The list of these
-# modules is saved as a variable, called $(x.module_list), where x is the
-# directory/suffix stripped part of the source file.  The beauty of this is that
-# in GNU make, $($(@F).module_list) gives you all the targets, and $^ gives you
-# all the prerequisites!
-#
-# The script also has the ability to not output for certain modules appearing in
-# "use" statements.  This is probably a good idea if you use modules supplied by
-# your compiler vendor and you don't want to tell "make" to go searching system
-# directories for them.  By default, "service_routines" is not output in
-# dependency lists (this comes with Lahey's LF95). By default, only "use"
-# statements which are in lower case are included in the dependency list.
-
-#*******************************************************************************
 # Usage :
 #
-#  perl -w make_dependencies.perl [-uc] [-skip_use mod_name] \\
-#      ... [-skip_use mod_name] [-prog_ext name] [-mod_ext name] \\
-#      [-obj_ext name] [-I include_dir] -o outfile -src infile
+#  perl -w make_dependencies.perl 
+#     [-uc] 
+#     [-skip_use mod_name] ... [-skip_use mod_name] 
+#     [-prog_ext name] 
+#     [-mod_ext name] 
+#     [-obj_ext name] 
+#     [-I include_dir] 
+#     -o outfile 
+#     -src infile
 #
 #  Where :
 #
-#     "-uc"                  : means translate module names to uppercase.
-#                              (This is for Cray Unicos and SGI).
-#     "-skip_use mod_name"   : means USE statements which reference
-#                              module "mod_name" should be ignored.  No rules
-#                              are produced for these.  To ignore multiple
-#                              modules, include multiple "skip_use" arguments
-#     "-prog_ext name"       : specify extension "name" for programs.
-#                              Default is exe.
-#     "-mod_ext name"        : specify extension "name" for modules.
-#                              Default is mod.
-#     "-obj_ext name"        : specify extension "name" for objects.
-#                              Default is o.
-#     "-I include_dir        : Use "include_dir" to search for included files
-#     "-o outfile"           : Output dependency information to file "outfile"
-#     "-src infile"          : Process source file "infile"
-
-#*******************************************************************************
-# $Id$ #
+#     -uc                  : means translate module names to uppercase.
+#                            (This is for Cray Unicos and SGI).
+#
+#     -skip_use mod_name   : means USE statements which reference
+#                            module "mod_name" should be ignored.  No rules
+#                            are produced for these.  To ignore multiple
+#                            modules, include multiple "skip_use" arguments
+#
+#     -prog_ext name       : specify extension "name" for programs.
+#                            Default is exe. If not defined, assume we
+#                            are producing and object file dependency.
+#
+#     -mod_ext name        : specify extension "name" for modules.
+#                            Default is mod.
+#
+#     -obj_ext name        : specify extension "name" for objects.
+#                            Default is o.
+#
+#     -I include_dir       : Use "include_dir" to search for included files
+#
+#     -o outfile"          : Output dependency information to file "outfile"
+#
+#     -src infile"         : Process source file "infile"
+#
+# Notes:
+#
+# . The script looks for "program", "module", "use", "include" and
+#   "#include" keywords.
+#
+# . The object file that is made from the source file has the same name as the
+#   source file but with ".o" extension.  Object files are prefixed
+#   with the $(objdir) directory. 
+#
+# . If the source file is for a program, then the extension is ".exe"
+#   or what is overriden with the -prog_ext argument. Program files
+#   are prefixed with the $(bindir) directory.  
+#
+# . The default module extension is ".mod", but this can be overridden
+#   with the -mod_ext command line argument. Module files are prefixed
+#   with the $(moddir) directory.
+#
+# . By default, module names from "use" statements are output in
+#   lowercase, but this can be overridden using -uc for those
+#   compilers that do uppercase instead.
+#
+# . The script can handle multiple modules in the source file, in which case they
+#   are all targets in the single "make" dependency rule.  The list of
+#   these modules is saved as a variable, called $(x.module_list),
+#   where x is the directory-and-suffix-stripped part of the source file.
+#   The beauty of this is that in GNU make, $($(@F).module_list) gives
+#   you all the targets, and $^ gives you all the prerequisites!
+#
+# . The script also has the ability to *not* output for certain
+#   modules appearing in "use" statements. By default, only "use"
+#   statements which are in lower case are included in the dependency
+#   list. This is a good idea if you use modules supplied by your
+#   compiler vendor and you don't want to tell "make" to go searching
+#   system directories for them.  By default, "service_routines" is
+#   not output in dependency lists (this comes with Lahey's LF95). 
+#
 # Copyright 2002 Daniel Grimwood, <reaper@theochem.uwa.edu.au>
+# Copyright 2012 Dylan Jayatilaka
 #
 # Thanks to :
 #   Ted Stern <stern@cray.com> for comments and a script to hack.
 #   Michael Wester <wester@math.unm.edu> for ideas from makemake.perl, via Ted.
-#*******************************************************************************
+#
+# $Id$ #
+# ============================================================================
+
 
 use File::Basename;
 
-#*******************************************************************************
-# Default settings.
+# ================
+# Default settings
+# ================
 
-# default to lowercase module file basename.
 $ModCase=LCase;
+# Default to lowercase module file basename.
 
-# default program extension.
 $prog_ext="exe";
+# Default program extension.
 
-# default module extension.
 $mod_ext="mod";
+# Default module extension.
 
 # default object extension.
 $obj_ext="o";
@@ -88,8 +109,9 @@ $obj_ext="o";
 push @skip_USE, service_routines;
 push @skip_USE, SERVICE_ROUTINES;
 
-#*******************************************************************************
-# Argument parsing.
+# ================
+# Argument parsing
+# ================
 
 $argerr=0;
 $argerr=1 if ($#ARGV+1==0);
@@ -110,12 +132,14 @@ while (@ARGV) {
     }
 }
 
-#*******************************************************************************
-# Error checks.
+# ============
+# Error checks
+# ============
 
 $prog_ext ne '' || do {$argerr=1; warn "Error : -prog_ext flag is null string\n"};
-$obj_ext ne '' || do {$argerr=1; warn "Error : -obj_ext flag is null string\n"};
-$mod_ext ne '' || do {$argerr=1; warn "Error : -mod_ext flag is null string\n"};
+$obj_ext  ne '' || do {$argerr=1; warn "Error : -obj_ext flag is null string\n"};
+$mod_ext  ne '' || do {$argerr=1; warn "Error : -mod_ext flag is null string\n"};
+
 defined $filename || do {$argerr=1; warn "Error : -src flag not supplied\n"};
 defined $depsfile || do {$argerr=1; warn "Error : -o flag not supplied\n"};
 
@@ -146,9 +170,9 @@ if ($argerr==1) {
         "\t\"-src infile\"\t\t: Process source file \"infile\"\n");
 }
 
-#*******************************************************************************
-# now to construct all the variables and lists.
-#*******************************************************************************
+# ============================================
+# Now to construct all the variables and lists
+# ============================================
 
 undef @incs;
 undef @modules;
@@ -157,29 +181,34 @@ undef @moddefs;
 undef $prog;
 undef @modlist;
 
-# get the base part of the file.  (minus extension and directory).
+# Get the base part of the file. 
+# (minus extension and directory).
 $base = basename($filename);
 $base =~ s/\.[^\.]+$//;
 
-# Search for includes and USE calls:
-&get_includes($filename);
+# Process the $filename for used modules, module definitions, 
+# and program statements
+&process_fortran_file($filename);
 
 # Recursively search include files.
 if (defined @incs) {
   $n=0;
   while ($n < scalar(@incs)) {
-    &get_includes($incs[$n]);
+    &process_fortran_file($incs[$n]);
   } continue { $n++; }
 }
 
-# if not a "program" statement, then assume we are producing an object file.
+# If not a "program" statement, then 
+# assume we are producing an object file.
 if (! defined $prog) {
   $prog_ext = $obj_ext;
 }
 
-# If modules are defined, create macros for modules and a combined object+module
+# If modules are defined, create macros 
+# for modules and a combined object+module
 # target rule:
 if (defined @moddefs) {
+
     # @modlist stores module names with .$mod_ext on the end
     foreach $module (&uniq(@moddefs)) {
         ($name = $module) =~ s/$/.$mod_ext/;
@@ -198,30 +227,38 @@ if (defined @moddefs) {
     }
 }
 
-# define the main target file to be compiled.
+# Define the main target file to be compiled.
 ($fullfile = $base) =~ s/$/.$prog_ext/;
+
 if (defined $prog) {
- # output the program with directory $(bindir).
+  
+  # Output the program with directory $(bindir).
   $fullfile =~ s/^/\$(bindir)\//;
+
 } else {
- # output the object with directory $(objdir).
+
+  # Output the object with directory $(objdir).
   $fullfile =~ s/^/\$(objdir)\//;
+
 }
 
-#*******************************************************************************
-# open the dependency output file for writing.
-#*******************************************************************************
+# ===========================================
+# Open the dependency output file for writing
+# ===========================================
 
 open(OUTFILE,">" . $depsfile) or die "Cannot open $depsfile for writing.\n";
 
 if (defined @modlist) {
+
     # This defines the macro $(base.prog_ext.module_list) as the list of modules
     # produced.  Also, macros like $(modname.$mod_ext.module_list) are also
     # produced which contain the same information.
     print OUTFILE "#Variables containing complete list of generated modules:\n";
     $word = "$base.$prog_ext.module_list :=";
+
     &PrintWords($word, @modlist);
-      print OUTFILE "\n";
+    print OUTFILE "\n";
+
     foreach $module (@moddefs) {
       $name = $module . ".module_list";
       $word = "$name :=";
@@ -240,18 +277,24 @@ if (defined @modlist) {
     print OUTFILE "\n";
 }
 
-# Print out extra dependencies for the object file beyond the default:
+# Print out extra dependencies for the 
+# object file beyond the default:
+
 if (defined @incs || defined @modules) {
+
     $skipUSE = &$ModCase( join(" ",@skip_USE) );
+
     foreach $module (&uniq(@modules)) {
         $a = $skipUSE;
         next if ($a =~ /$module/ );
         ($name = $module) =~ s/.*/\$\(moddir)\/$&.$mod_ext/;
         push(@dependencies, $name);
     }
+
 }
 
 print OUTFILE "#Dependencies of all files produced:\n";
+
 if (defined @modlist) {
   $word = "$fullfile \$($base.$prog_ext.module_list) :";
   &PrintWords($word, $filename, @dependencies, &uniq(@incs));
@@ -265,15 +308,13 @@ close(OUTFILE);
 
 
 
-#*******************************************************************************
-# Now the subroutine definitions....
-#*******************************************************************************
 
-#*******************************************************************************
+# ===================================================
 # &PrintWords(list);
 # print the list to span multiple lines if necessary.
 # Based on the one from makemake.perl.
-#
+# ===================================================
+
 sub PrintWords {
     my ($columns, $wordlength);
     $columns = 79;
@@ -293,9 +334,11 @@ sub PrintWords {
     }
 }
 
-#*******************************************************************************
+# ===================================================
 # &uniq(list)
 # return the list in original order minus duplicates.
+# ===================================================
+
 sub uniq {
     my @words;
     foreach $x (@_) {
@@ -305,28 +348,37 @@ sub uniq {
     return @words;
 }
 
-#*******************************************************************************
+# ============================
 # &UCase(string)
 # convert string to upper case
+# ============================
+
 sub UCase {
     return uc($_[0]);
 }
 
-#*******************************************************************************
+# ============================
 # &LCase(string)
 # convert string to lower case
+# ============================
+
 sub LCase {
     return lc($_[0]);
 }
 
-#*******************************************************************************
-# &get_includes(filename)
-# Search for include files in the given file, and add them to the @incs array.
-# Also retrieve from this file other dependency information.
-sub get_includes {
+# ============================================================================
+# &process_fortran_filr(filename)
+# Search $filename for used modules, program statemenets, and included
+# files. Store these in arrays.
+# ============================================================================
+
+sub process_fortran_file {
+
   my ($dir,$filehead,$inc,$inchead,$use,$usehead);
 
   $dir = "";
+
+  # Find which directory the file is located
   $filehead = "";
   if (! open(FILE, $_[0])) {             # include file doesnt exist
     foreach $dir (@include_dirs) {       # look in the include paths
@@ -335,11 +387,18 @@ sub get_includes {
     }
     if ($filehead eq "") { die "Cannot find file $_[0]\n"; }
   }
+
   my $line;
+
+  # Find which directory the include-files are located.
+  # Place them in @incs
   while ($line=<FILE>) {
+
     if ($line =~ m'include'io &&
         $line =~ /^\s*(\#|\?\?)*\s*include\s+[\"\']([^\"\']+)[\"\']/io) {
+
        $inc = $2;
+
        $inchead="";
        if (! open(INC, $inc)) {          # include file doesnt exist
           foreach $dir (@include_dirs) { # look in the include paths
@@ -350,17 +409,27 @@ sub get_includes {
        }
        close(INC);
        push(@incs, $inchead . $inc);
+
     }
-    $line =~ /^\s*use\s+(\w+)/o &&       # only lower case "use" statements are included
+
+    # Store only lower case "used" module names in @modules
+    $line =~ /^\s*use\s+(\w+)/o &&       
             do {push(@modules, &$ModCase("$1"))};
+
+    # Store module names in @moddefs
     $line =~ /^\s*module\s+(\w+)/io && 
             do {push(@moddefs, &$ModCase("$1")) if not (lc($1) eq "procedure")};
+
+    # Store program title in $prog
     $line =~ /^\s*program\s+(\w+)/io && 
             do {$prog = &$ModCase($1)};
   }
+
   close(FILE);
+
   # Remove redundant entries.
   @incs = &uniq(@incs);
   @modules = &uniq(@modules);
+
 }
 
--- trunk/tonto/scripts/make_foo_dependencies.pl
+++ trunk/tonto/scripts/make_foo_dependencies.pl
@@ -1,116 +1,186 @@
-# This perl script adds dependencies for Fortran 95 files whose .foo files have
-# get_from directives.  This script is specific to Tonto.
+# ==================================================================
 #
-#*******************************************************************************
-# Usage :
+# This perl script adds dependencies for Fortran 95 files whose 
+# .foo files have get_from(...) directives. It is specific to the
+# foo langauge and Tonto.
 #
-#  perl -w make_get_dependencies.perl -o outfile -src infile
+# Usage:
 #
-#  Where :
+# perl -w make_foo_dependencies.pl -o outfile -src foofile
 #
-#     "-o outfile"           : Output dependency information to file "outfile"
-#     "-src infile"          : Process source file "infile"
+# where:
+#
+# -o   depfile : places foo-dependency info in file "depfile"
+# -src foofile : causes processing source .foo file "foofile"
+#
+# (c) Copyright 2003-- Daniel Grimwood, Dylan Jayatilaka
+# (c) Copyright 2012-- Dylan Jayatilaka
+#
+# $Id$
+# ==================================================================
 
-#*******************************************************************************
-# $Id$ #
-# Copyright 2003 Daniel Grimwood, <reaper@theochem.uwa.edu.au>
-#*******************************************************************************
 
 use File::Basename;
 
-my $module_name; # the name of the current module
+# The name of the current module
+my $module_name; 
 
-#*******************************************************************************
-# Argument parsing.
+# Where the get_from's are read from ...
+my $foofile;
 
-$argerr=0;
-$argerr=1 if ($#ARGV+1==0);
+# Where the dependencies are deposited ...
+my $depfile;
 
+# ============================
+# Parse command-line arguments
+# ============================
+
+# Argument error?
+my $argerr = 0;
+
+# Must be at least one argument
+$argerr = 1 if ($#ARGV+1==0);
+
+# Extract command-line arguments
 while (@ARGV) {
-    $arg=shift;
+
+    $arg = shift;
+
     SWITCH: for ($arg) {
-        /^-src$/       && do { $filename=shift; last; };
-        /^-o$/         && do { $depsfile=shift; last; };
+        /^-src$/       && do { $foofile = shift; last; };
+        /^-o$/         && do { $depfile = shift; last; };
         warn "Error : unexpected argument $arg\n";
-        $argerr=1;
+        $argerr = 1;
     }
+
 }
 
-$filename =~ s/\\{/{/g;
-$depsfile =~ s/\\{/{/g;
+# Unprotect curly braces
+$foofile =~ s/\\{/{/g;
+$depfile =~ s/\\{/{/g;
 
-#*******************************************************************************
-# Error checks.
+# Any errors?
+defined $foofile || do {$argerr = 1; warn "Error : -src flag not supplied\n"};
+defined $depfile || do {$argerr = 1; warn "Error : -o flag not supplied\n"};
 
-defined $filename || do {$argerr=1; warn "Error : -src flag not supplied\n"};
-defined $depsfile || do {$argerr=1; warn "Error : -o flag not supplied\n"};
+# ===================================================
+# Print out standard help message if there's an error
+# ===================================================
 
-# Print out standard help message if there's an error.
 if ($argerr==1) {
+
     die(
         "\nUsage:\n",
-        "\t perl -w make_dependencies.perl -o outfile -src infile\n\n",
-        "Where :\n",
-        "\t\"-o outfile\"\t\t: Output dependency information to file\n",
-        "\t\t\t\t  \"outfile\"\n",
-        "\t\"-src infile\"\t\t: Process source file \"infile\"\n");
+        "\t perl -w make_dependencies.perl -o outfile -src filename\n\n",
+        "where :\n",
+        "\t\"-o depfile\"\t\t: Output dependency information to file\n",
+        "\t\t\t\t  \"depfile\"\n",
+        "\t\"-src foofile\"\t\t: Process foo source file \"foofile\"\n");
 }
 
-#*******************************************************************************
-# now to construct all the variables and lists.
-#*******************************************************************************
+# ===========================================
+# Get and store all the get_from dependencies
+# ===========================================
 
+# Open the file
+open(FILE, $foofile) or die "Cannot find file $_[0]\n";
+
+# The (non-unique) list of get_froms
+my    @gets;
 undef @gets;
 
-# get the base part of the file.  (minus extension and directory).
-$base = basename($filename);
-$base =~ s/\.[^\.]+$//;
+# Loop over file lines ...
+while (<FILE>) {
 
-&get_deps($filename);
-
-if (defined @gets) {
-  foreach $get (&uniq(@gets)) {
-    ($name = $get) =~ s/$/.foo/;
-    push (@getlist,'$(foodir)/' . $name);
+  # Extract (lower case) module name
+  if (/^module\s+([A-Z][\w{,}.]*[\w}])/) {
+     $module_name = lc($1);
   }
+
+  # Extract get_from ...
+  if (/get_from/) {
+
+    if (/(?:.+):::(?:.+)get_from\(\s*([A-Z][A-Z0-9_{,}.]*[A-Z0-9_}])/) {
+
+       # get_from(MODULE:xxxx)
+       $x = $1; 
+       push(@gets, lc($x));
+       # print "x = $x\n";
+
+    } elsif (/(?:.+):::(?:.+)get_from\(/) { 
+
+       # get_from(xxxx)
+       push(@gets, $module_name);
+       # print "m = $module_name\n";
+
+    }
+
+  }
+
 }
 
-#*******************************************************************************
-# open the dependency output file for writing.
-#*******************************************************************************
+close(FILE);
 
-open(OUTFILE,">" . $depsfile) or die "Cannot open $depsfile for writing.\n";
+# =============================
+# Make the unique get_from list
+# =============================
 
-print OUTFILE "#get_from dependencies for $filename:\n";
-if (defined @getlist) {
-  $word = '$(f95dir)/' . "$base" . '.$(FSUFFIX) :';
-  &PrintWords($word, @getlist);
+my    @getlist;
+undef @getlist;
 
-  print OUTFILE "\n";
-  $word = '$(htmldir)/' . "$base" . '.html :';
-  &PrintWords($word, @getlist);
+if (@gets) {
+
+   foreach $get (&unique(@gets)) {
+      ($name = $get) =~ s/$/.foo/;
+      push (@getlist,'$(foodir)/' . $name);
+   }
+
 }
 
-print OUTFILE "\n\n";             # Add newline to .dep file
+# ==============================
+# Print out the foo dependencies
+# ==============================
+
+open(OUTFILE,">" . $depfile) or die "Cannot open $depfile for writing.\n";
+
+print OUTFILE "# get_from() dependencies for $foofile:\n";
+
+if (@getlist) {
+
+   # The base part of the foofile (minus directory).
+   my $base;
+   $base = basename($foofile);
+ # $base =~ s/\.[^\.]+$//;
+
+   $word = '$(f95dir)/' . "$base" . '.$(FSUFFIX) :';
+   &PrintWords($word, @getlist);
+
+   print OUTFILE "\n";
+   $word = '$(htmldir)/' . "$base" . '.html :';
+   &PrintWords($word, @getlist);
+}
+
+print OUTFILE "\n\n";             
+
 close(OUTFILE);
 
 
-
-#*******************************************************************************
-# Now the subroutine definitions....
-#*******************************************************************************
-
-#*******************************************************************************
-# &PrintWords(list);
-# print the list to span multiple lines if necessary.
+# ==================================================================
+# &PrintWords(@list);
+# Print the @list to span multiple lines if necessary.
 # Based on the one from makemake.perl.
-#
+# ==================================================================
+
 sub PrintWords {
+
     my ($columns, $wordlength);
+
     $columns = 79;
 
     foreach $word (@_) {
+
         $wordlength = length($word);
+
         if ($columns == 79) {
             print OUTFILE "$word";
             $columns -= $wordlength + 1;
@@ -121,49 +191,27 @@ sub PrintWords {
            print OUTFILE " \\\n\t$word";
            $columns = 71 - $wordlength;
         }
+
     }
+
 }
 
-#*******************************************************************************
-# &uniq(list)
-# return the list in original order minus duplicates.
-sub uniq {
+# ==================================================================
+# &unique(@list)
+# Return the @list in original order minus duplicates.
+# ==================================================================
+
+sub unique {
+
     my %words;
+
     foreach $x (@_) {
       $words{$x} = 1;
     # $_ = join(" ",@words);
     # if (! /\b\Q$x\E\b/) { push(@words,$x); }
     }
+
     return (keys %words);
-}
-
-#*******************************************************************************
-# &get_deps(filename)
-sub get_deps {
-  my ($dir,$filehead,$inc,$inchead,$use,$usehead);
-
-  open(FILE, $_[0]) or die "Cannot find file $_[0]\n";
-
-print STDERR @gets;
-
-  while (<FILE>) {
-
-    if (/^module\s+([A-Z][\w{,}.]*[\w}])/) {
-       $module_name = lc($1);
-    }
-
-    if (/get_from/) {
-      if (/(?:.+):::(?:.+)get_from\(\s*([A-Z][\w{,}.]*[\w}])\s*[:,)]/) {
-         $x = $1; 
-         push(@gets, lc($x));
-      } elsif (/(?:.+):::(?:.+)get_from\(/) { 
-         push(@gets, $module_name);
-      }
-    }
-
-  }
-
-  close(FILE);
 
 }
 
